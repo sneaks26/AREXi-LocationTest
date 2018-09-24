779c294e111657a43390583f7ab4982de4fe084e
Shuyen <shuyenphoon@gmail.com>
2018-09-24 19:55:30 +1000

    Created coroutine examples

--- Assets/Script/CoRoutine_Example.cs
+++ Assets/Script/CoRoutine_Example.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoRoutine-Example : MonoBehaviour {
+public class CoRoutine_Example : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public Transform Cube;
+    public Renderer CubeRenderer;
+
+    //how long the fade takes, in seconds
+    public float duration = 2f;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(FadeColor(Color.black));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    // Make the cube do things in a CoRoutine
+    IEnumerator TestCoroutine(){
+        Cube.Translate(0.5f, 0f, 0f);
+        yield return new WaitForSeconds(1f);
+        Cube.Translate(-0.5f, 0f, 0f);
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(TestCoroutine());
+    }
+
+    IEnumerator FadeColor(Color TargetColor){
+        //starting with this colour
+        Color StartColor = Color.magenta;
+
+        float t = 0;
+        while(t < 1f){
+
+            //change the colour
+            CubeRenderer.material.color = Color.Lerp(StartColor, TargetColor, t);
+
+            //increase the increment
+            t += Time.deltaTime / duration;
+
+            //tell the script to wait for next frame
+            yield return null;
+        }
+        //Color Example = Color.Lerp();
+        yield break;
+    }
 }
